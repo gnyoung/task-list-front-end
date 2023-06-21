f0106372ae7210b63f70462c5bead72e501813e1
anclark686 <anclark686@gmail.com>
2023-06-20 21:51:13 -0400

    adding delete, and patch methods

--- src/App.js
+++ src/App.js
@@ -1,42 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
 const App = () => {
-  const [taskData, setTaskData] = useState([
-    {
-      id: 1,
-      title: 'Mow the lawn',
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: 'Cook Pasta',
-      isComplete: true,
-    },
-  ]);
+  const baseUrl = 'http://127.0.0.1:5000/tasks';
+
+  const [taskData, setTaskData] = useState([]);
+
+  const taskDataConvert = (res) => {
+    return res.map((task) => {
+      return { ...task, isComplete: task.is_complete};
+    });
+  };
+
+  const getTaskData = () => {
+    axios.get(baseUrl)
+    .then(res => {
+      setTaskData(taskDataConvert(res.data));
+      console.log(taskDataConvert(res.data));
+    })
+    .catch((err) => console.log(err));
+    
+  };
+
+  useEffect(() => {
+    getTaskData();
+  }, []);
   
   const updateTaskData = updatedTaskData => {
+    let completeOrIncomplete;
     const tasks = taskData.map(task => {
       if (task.id === updatedTaskData.id) {
+        completeOrIncomplete = updatedTaskData.isComplete;
         return updatedTaskData;
       } else {
         return task;
       }
     });
     setTaskData(tasks);
+
+    if (completeOrIncomplete) {
+      axios
+        .patch(`${baseUrl}/${updatedTaskData.id}/mark_complete`)
+        .then((res) => console.log("Back end response", res.data))
+        .catch((err) => console.log(err));
+    } else {
+      axios
+        .patch(`${baseUrl}/${updatedTaskData.id}/mark_incomplete`)
+        .then((res) => console.log("Back end response", res.data))
+        .catch((err) => console.log(err));
+    }
+    
   };
 
   const onDeleteItem = id => {
-    // this didn't work
-    // const tasks = taskData.map(task => {
-    //   if (task.id !== id) {
-    //     return task;
-    //   }
-    // });
-    const tasks = [...taskData];
-    const updatedTasks = tasks.filter((item) => item.id !== id);
-    setTaskData(updatedTasks);
+    // this was surprisingly close to what I had!
+    setTaskData((prev) => prev.filter((task) => task.id !== id));
+
+    axios
+      .delete(`${baseUrl}/${id}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   };
   
   return (
